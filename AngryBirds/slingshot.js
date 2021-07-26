d5ac9cc2089f33a1d9eecd8f756618c3fa778950
leminhhoang210@gmail.com <leminhhoang210@gmail.com>
2021-07-26 21:53:35 +0700

    update

--- AngryBirds/slingshot.js
+++ AngryBirds/slingshot.js
@@ -33,8 +33,8 @@ Example.slingshot = function() {
 
     var ground = Bodies.rectangle(395, 600, 1010, 50, { isStatic: true }),
         rockOptions = { density: 0.004 },
-        rock = Bodies.polygon(170, 450, 8, 20, rockOptions),
-        anchor = { x: 170, y: 450 },
+        rock = Bodies.polygon(170, 370, 8, 20, rockOptions),
+        anchor = { x: 170, y: 370 },
         elastic = Constraint.create({
             pointA: anchor,
             bodyB: rock,
@@ -43,60 +43,57 @@ Example.slingshot = function() {
     rock.label = 'rock';
 
     var pyramid = Composites.pyramid(500, 380, 9, 10, 0, 0, function(x, y)
-    {return Bodies.rectangle(x, y, 35, 40)});
+    {return Bodies.rectangle(x, y, 30, 30, { density: 0.004})});
 
-    var ground2 = Bodies.rectangle(620, 200, 140, 20, { isStatic: true });
+    var ground2 = Bodies.rectangle(680, 200, 30, 20, { isStatic: true });
 
     var diamondOptions = {
             density: 0.004,
             label: 'diamond',
             render: {
-                strokeStyle: '#ffffff',
-                sprite: {
-                    texture: './rock.png'
-                }
+                strokeStyle: 'white',
+                fillStyle: 'white'
             },
         },
-        diamond = Bodies.polygon(656.5,365, 8, 20, diamondOptions),
-        smallDiamondOptions = {
+        diamond = Bodies.rectangle(635,380, 30, 30, diamondOptions),
+        particleOptions = {
             density: 0.00001,
-            render: {
-                fillStyle: 'white'
-            }
+            render: {fillStyle: 'white'}
         };
-    diamond.label = 'diamond';
-    var getDiamond = function(x, y) {
-        let temp = Bodies.polygon(x,y, 8, 20, diamondOptions);
-        temp.label = 'diamond';
-        return temp;
-    };
-    var pyramid2 = Composites.pyramid(557, 0, 5, 10, 0, 0, getDiamond);
 
+    var diamond2 = Bodies.polygon(680,180, 8, 20, diamondOptions);
+
+    World.add(engine.world, [ground, ground2, diamond2, pyramid, rock, elastic, diamond]);
+
+    var threshold = 30;
     Events.on(engine, 'collisionStart', function(event) {
         var pairs = event.pairs;
         for (var i = 0, j = pairs.length; i != j; ++i) {
             var pair = pairs[i];
-            collided = undefined;
-            if (pair.bodyA.label === "rock" && pair.bodyB.label === "diamond"){
-                collided = pair.bodyB;
-            } else if (pair.bodyA.label === "diamond" && pair.bodyB.label === "rock"){
-                collided = pair.bodyA;
+            let bodyA = undefined,
+                bodyB = undefined;
+            if (pair.bodyB.label.startsWith("diamond"))   {
+                bodyA = pair.bodyB;
+                bodyB = pair.bodyA;
+            } else if (pair.bodyA.label.startsWith("diamond")) {
+                bodyA = pair.bodyA;
+                bodyB = pair.bodyB;
             }
-            if (collided) {
-                World.remove(world, collided);
-                pos = collided.position;
-                World.add(engine.world, Composites.stack(pos.x, pos.y, 6, 6, 0, 0, function(x, y)
-                {return Bodies.circle(x,y, 3, smallDiamondOptions)}));
+            if (bodyA) {
+                let velocity = bodyB.velocity;
+                if (Math.sqrt(velocity.x*velocity.x + velocity.y*velocity.y) * bodyB.mass > threshold) {
+                    pos = bodyA.position;
+                    World.remove(world, bodyA);
+                    World.add(engine.world, Composites.stack(pos.x, pos.y, 6, 6, 0, 0, function(x, y)
+                    {return Bodies.circle(x,y, 3, particleOptions)}));
+                }
             }
         }
     });
 
-
-    World.add(engine.world, [ground, pyramid, ground2, pyramid2, rock, elastic, diamond]);
-
     Events.on(engine, 'afterUpdate', function() {
-        if (mouseConstraint.mouse.button === -1 && (rock.position.x > 190 || rock.position.y < 430)) {
-            rock = Bodies.polygon(170, 450, 7, 20, rockOptions);
+        if (mouseConstraint.mouse.button === -1 && (rock.position.x > 190 || rock.position.y < 350)) {
+            rock = Bodies.polygon(170, 370, 7, 20, rockOptions);
             rock.label = 'rock';
             World.add(engine.world, rock);
             elastic.bodyB = rock;
@@ -123,7 +120,7 @@ Example.slingshot = function() {
     // fit the render viewport to the scene
     Render.lookAt(render, {
         min: { x: 0, y: 0 },
-        max: { x: 800, y: 600 }
+        max: { x: 1000, y: 600 }
     });
 
     // context for MatterTools.Demo
