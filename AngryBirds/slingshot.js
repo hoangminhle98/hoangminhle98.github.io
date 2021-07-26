113b625367c144f2afc05c88ec520c355d1457d3
leminhhoang210@gmail.com <leminhhoang210@gmail.com>
2021-07-26 20:08:41 +0700

    diamond breaks

--- AngryBirds/slingshot.js
+++ AngryBirds/slingshot.js
@@ -40,21 +40,64 @@ Example.slingshot = function() {
             bodyB: rock,
             stiffness: 0.05
         });
+    rock.label = 'rock';
 
-    var pyramid = Composites.pyramid(500, 300, 9, 10, 0, 0, function(x, y)
-    {return Bodies.rectangle(x, y, 25, 40)});
+    var pyramid = Composites.pyramid(500, 380, 9, 10, 0, 0, function(x, y)
+    {return Bodies.rectangle(x, y, 35, 40)});
 
-    var ground2 = Bodies.rectangle(610, 250, 200, 20, { isStatic: true });
+    var ground2 = Bodies.rectangle(620, 200, 140, 20, { isStatic: true });
 
-    var pyramid2 = Composites.pyramid(550, 0, 5, 10, 0, 0, function(x, y)
-    {return Bodies.rectangle(x, y, 25, 40)});
+    var diamondOptions = {
+            density: 0.004,
+            label: 'diamond',
+            render: {
+                strokeStyle: '#ffffff',
+                sprite: {
+                    texture: './rock.png'
+                }
+            },
+        },
+        diamond = Bodies.polygon(656.5,365, 8, 20, diamondOptions),
+        smallDiamondOptions = {
+            density: 0.00001,
+            render: {
+                fillStyle: 'white'
+            }
+        };
+    diamond.label = 'diamond';
+    var getDiamond = function(x, y) {
+        let temp = Bodies.polygon(x,y, 8, 20, diamondOptions);
+        temp.label = 'diamond';
+        return temp;
+    };
+    var pyramid2 = Composites.pyramid(557, 0, 5, 10, 0, 0, getDiamond);
+
+    Events.on(engine, 'collisionStart', function(event) {
+        var pairs = event.pairs;
+        for (var i = 0, j = pairs.length; i != j; ++i) {
+            var pair = pairs[i];
+            collided = undefined;
+            if (pair.bodyA.label === "rock" && pair.bodyB.label === "diamond"){
+                collided = pair.bodyB;
+            } else if (pair.bodyA.label === "diamond" && pair.bodyB.label === "rock"){
+                collided = pair.bodyA;
+            }
+            if (collided) {
+                World.remove(world, collided);
+                pos = collided.position;
+                World.add(engine.world, Composites.stack(pos.x, pos.y, 6, 6, 0, 0, function(x, y)
+                {return Bodies.circle(x,y, 3, smallDiamondOptions)}));
+            }
+        }
+    });
 
 
-    World.add(engine.world, [ground, pyramid, ground2, pyramid2, rock, elastic]);
+    World.add(engine.world, [ground, pyramid, ground2, pyramid2, rock, elastic, diamond]);
 
     Events.on(engine, 'afterUpdate', function() {
         if (mouseConstraint.mouse.button === -1 && (rock.position.x > 190 || rock.position.y < 430)) {
             rock = Bodies.polygon(170, 450, 7, 20, rockOptions);
+            rock.label = 'rock';
             World.add(engine.world, rock);
             elastic.bodyB = rock;
         }
